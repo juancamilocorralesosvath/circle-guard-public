4485be076791c0a8e2504a585566e769132958a9
juancamilocorralesosvath <juancamilocorra@gmail.com>
2026-05-11 18:10:11 -0500

    fix: notification tests

--- services/circleguard-notification-service/src/test/java/com/circleguard/notification/service/RoomReservationServiceTest.java
+++ services/circleguard-notification-service/src/test/java/com/circleguard/notification/service/RoomReservationServiceTest.java
@@ -3,11 +3,22 @@ package com.circleguard.notification.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.ActiveProfiles;
 import java.util.concurrent.CompletableFuture;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class RoomReservationServiceTest {
+
+    @MockBean
+    private JavaMailSender mailSender;
+
+    @MockBean
+    private KafkaTemplate<String, Object> kafkaTemplate;
 
     @Autowired
     private RoomReservationService roomReservationService;
--- services/circleguard-notification-service/src/test/java/com/circleguard/notification/service/TemplateServiceTest.java
+++ services/circleguard-notification-service/src/test/java/com/circleguard/notification/service/TemplateServiceTest.java
@@ -3,10 +3,21 @@ package com.circleguard.notification.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.ActiveProfiles;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class TemplateServiceTest {
+
+    @MockBean
+    private JavaMailSender mailSender;
+
+    @MockBean
+    private KafkaTemplate<String, Object> kafkaTemplate;
 
     @Autowired
     private TemplateService templateService;
